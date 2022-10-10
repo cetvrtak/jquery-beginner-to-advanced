98bdf0e884832bb4ea0161b8319b146534e5c826
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-10-10 13:31:22 +0200

    Section 3

---
+++ Section 2/base/js/script.js
@@ -0,0 +1,81 @@
+$(function () {
+  // jQuery goes here...
+  // Uncomment this line to fade out the red box on page load
+  // $(".red-box").fadeOut(2000);
+  // $(".green-box").fadeOut(2000);
+  // $(".red-box").fadeIn(1000);
+  // $(".red-box").fadeTo(1000, 0.5);
+});
+
+// $(function() {
+//   $(".red-box").fadeTo(3000, 0.2);
+//   $(".green-box").fadeTo(2000, 0.5);
+//   $(".blue-box").fadeTo(1000, 0.8);
+
+//   $(".green-box").fadeToggle();
+//   $(".green-box").fadeToggle();
+// });
+
+// $(function () {
+//   $(".blue-box").hide();
+//   $(".blue-box").hide(1000);
+//   $(".blue-box").show(1000);
+//   $(".blue-box").show();
+//   $(".blue-box").toggle();
+// });
+
+// $(function () {
+//   $(".blue-box").slideUp(2000);
+//   $(".blue-box").slideDown(2000);
+//   $(".blue-box").slideToggle(2000);
+
+//   $("p").hide();
+//   $("p").slideDown(1000);
+// });
+
+// $(function () {
+//   $(".blue-box").animate(
+//     {
+//       "margin-left": "+=200px",
+//     },
+//     2000,
+//     "swing"
+//   );
+//   $(".blue-box").animate({ "margin-left": "-=200px" });
+// });
+
+// $(function () {
+//   $(".blue-box").animate(
+//     {
+//       marginLeft: "200px",
+//       opacity: "0",
+//       width: "50px",
+//       height: "50px",
+//       marginTop: "25px",
+//     },
+//     1000
+//   );
+
+//   $("p").animate(
+//     {
+//       fontSize: "20px",
+//     },
+//     1000
+//   );
+// });
+
+// $(function () {
+//   $(".red-box").fadeTo(1000, 0.2);
+//   $(".green-box").delay(1000).fadeTo(1000, 0.5);
+//   $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
+// });
+
+// $(function () {
+//   $(".red-box").fadeTo(1000, 0.2, () =>
+//     $(".green-box").fadeTo(1000, 0, () => $(".blue-box").fadeTo(1000, 0))
+//   );
+// });
+
+// $(() => {
+//   $(".lightbox").delay(500).fadeIn(1000);
+// });
---
+++ Section 3/base/js/script.js
@@ -0,0 +1,46 @@
+// $(function () {
+//   $("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("p:first").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li:last").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li:even").css("background-color", "rgba(180, 180, 30, 0.8)");
+
+//   $("input:text").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("input:email").css("background-color", "rgba(180, 180, 30, 0.8)"); // doesn't work
+//   $("input:radio").css("background-color", "rgba(180, 180, 30, 0.8)");
+// });
+
+// $(function () {
+//   $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").parents("body").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").parent().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").siblings().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").siblings("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").prev().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").next().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").prev().next().css("background-color", "rgba(180, 180, 30, 0.8)");
+
+//   $(":header").next().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("input:password").css("background-color", "rgba(180, 180, 30, 0.8)");
+// });
+
+// $(function () {
+//   $("#list").find("li").filter(":even").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("#list").children("li").filter(":even").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li")
+//     .filter((i) => i % 3 == 2)
+//     .css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li").first().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li").last().css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li").eq(7).css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li").eq(-2).css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li").not(":first").css("background-color", "rgba(180, 180, 30, 0.8)");
+//   $("li")
+//     .not((i) => i % 3 == 2)
+//     .css("background-color", "rgba(180, 180, 30, 0.8)");
+// });
